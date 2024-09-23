7e47de35e6cb45a14ee0cde72e0e375719d13fb2
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-09-23 10:57:11 +0200

    solve challenge 4

--- index.js
+++ index.js
@@ -29,96 +29,94 @@ function outer() {
 
 // CHALLENGE 1
 function createFunction() {
-	
-    function sayHello () {
-        console.log('hello');
-      
-    }
-    return sayHello;
-  }
+	function sayHello() {
+		console.log("hello");
+	}
+	return sayHello;
+}
   
-  /*** Uncomment these to check your work! ***/
-  const function1 = createFunction();
-  function1(); // => should console.log('hello');
-  
-  
-  // CHALLENGE 2
-  function createFunctionPrinter(input) {
-     function printInput () {
-      console.log(input);
-    }
-      
-    return printInput;
-  }
-  
-  /*** Uncomment these to check your work! ***/
+/*** Uncomment these to check your work! ***/
+// const function1 = createFunction();
+// function1(); // => should console.log('hello');
+
+
+// CHALLENGE 2
+function createFunctionPrinter(input) {
+    function printInput () {
+    console.log(input);
+}
+    
+return printInput;
+}
+
+/*** Uncomment these to check your work! ***/
 //   const printSample = createFunctionPrinter('sample');
 //   printSample(); // => should console.log('sample');
 //   const printHello = createFunctionPrinter('hello');
 //   printHello(); // => should console.log('hello');
   
   
-  // CHALLENGE 3
-  function outer() {
-    let counter = 0; // this variable is outside incrementCounter's scope
-    function incrementCounter () {
-      counter ++;
-      console.log('counter', counter);
-    }
-    return incrementCounter;
-  }
-  
-  const willCounter = outer();
-  const jasCounter = outer();
-  
-  // Uncomment each of these lines one by one.
-  // Before your do, guess what will be logged from each function call.
-  
-  /*** Uncomment these to check your work! ***/
+// CHALLENGE 3
+function outer() {
+let counter = 0; // this variable is outside incrementCounter's scope
+function incrementCounter () {
+    counter ++;
+    console.log('counter', counter);
+}
+return incrementCounter;
+}
+
+const willCounter = outer();
+const jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+/*** Uncomment these to check your work! ***/
 //   willCounter(); //counter1
 //   willCounter(); //counter2
 //   willCounter(); //counter3
-  
+
 //   jasCounter(); //counter1
 //   willCounter(); //counter4
-  
-  
-function addByX(x) {
-	let value;
 
+
+function addByX(x) {
 	function addInputByX(input) {
-		if (value !== undefined) {
-			return value;
-		} else {
-			value = x + input;
-			return value;
-		}
-		// console.log(x + input)
+		// console.log(x + input);
+		return x + input;
 	}
 	return addInputByX;
 }
-  
-  // /*** Uncomment these to check your work! ***/
-  const addByTwo = addByX(2);
+
+// /*** Uncomment these to check your work! ***/
+const addByTwo = addByX(2);
 //   addByTwo(1); // => should return 3
 //   addByTwo(2); // => should return 4
 //   addByTwo(3); // => should return 5
-  
+
 //   const addByThree = addByX(3);
 //   addByThree(1); // => should return 4
 //   addByThree(2); // => should return 5
-  
+
 //   const addByFour = addByX(4);
 //   addByFour(4); // => should return 8
 //   addByFour(5); // => should return 9
-  
-  
-  // CHALLENGE 4
-  function once(func) {
 
-      return func;
-  }
-  
+// CHALLENGE 4
+function once(func) {
+	let runOnce;
+	return function (x) {
+        if (runOnce !== undefined) {
+            console.log("This function has been called before")
+            return runOnce;
+		}
+        console.log('first time')
+        runOnce = func(x);
+        return runOnce;
+	};
+}
+
   // /*** Uncomment these to check your work! ***/
   const onceFunc = once(addByTwo);
   console.log(onceFunc(4));  // => should log 6
